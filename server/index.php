8b5cca27a184a2f13364e46ce0dcc40a25b85ae8
antonioasaro <antonio_asaro@hotmail.com>
2013-09-20 19:38:10 -0400

    Get input data & post it to database

--- server/index.php
+++ server/index.php
@@ -1,3 +1,37 @@
-<?PHP
-  print "It works."
-?>
+<html>
+<body>
+
+<form action="insert.php" method="post">
+  <table>
+    <tr>
+      <td align="right">Serial Number:</td>
+      <td align="left"><input type="text" name="serial_num" /></td>
+      <td align="left">// 12-digits on the watchback</td>
+    </tr>
+    <tr>
+      <td align="right">Location:</td>
+      <td align="left"><input type="text" name="location" /></td>
+      <td align="left">// e.g. Toronto,Canada</td>
+    </tr>
+    <tr>
+      <td align="right">Units:</td>
+      <td align="left"><input type="text" name="units" /></td>
+      <td align="left">// Metric or Imperial</td>
+    </tr>
+    <tr>
+      <td align="right">Stock1:</td>
+      <td align="left"><input type="text" name="stock1" /></td>
+      <td align="left">// e.g. IBM</td>
+    </tr>
+    <tr>
+      <td align="right">Stock2:</td>
+      <td align="left"><input type="text" name="stock2" /></td>
+      <td align="left">// e.g. GOOG</td>
+    </tr>
+  </table>
+  <br> <input type="submit">
+</form>
+
+</body>
+</html>
+
