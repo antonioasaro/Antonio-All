0a3eecf779023e6bef1cee06fa6e33dacf9e76d8
antonioasaro <antonio_asaro@hotmail.com>
2013-09-22 13:50:48 -0400

    Use table to format the <form>

--- server/index_db.php
+++ server/index_db.php
@@ -1,36 +1,20 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html><head><meta content="text/html; charset=ISO-8859-1" http-equiv="content-type"><title>Antonio All Settings</title>
-
-
-</head>
+<html><head><meta content="text/html; charset=ISO-8859-1" http-equiv="content-type"><title>Antonio All Settings</title></head>
 <body style="background-color: white; color: rgb(0, 0, 0);" alink="#ee0000" link="#0000ee" vlink="#551a8b">
-<span style="font-weight: bold; color: red;"><img style="width: 117px; height: 87px;" alt="icon" src="pebble_smartwatch.jpg"><br>
+<span style="font-weight: bold; color: red;"><img style="width: 117px; height: 87px;" alt="icon" src="pebble_smartwatch.jpg"><br><br>
 <span style="color: black; background-color: white;">Input
-your personal settings for the&nbsp;Antonio All watchface ...</span></span><br>
+your personal settings for the <small>"</small>Antonio All" watchface ...</span></span><br>
 <br>
-<form method="post" action="process_db.php" name="add">Serial
-Number:&nbsp;&nbsp; &nbsp;<input name="serial_num">
-&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
-<span style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; display: inline ! important; float: none;">//
-12-digits serial number on the watchback</span><br>
-Location:&nbsp;&nbsp;&nbsp; &nbsp; &nbsp;
-&nbsp;&nbsp; &nbsp; <input name="location">&nbsp;&nbsp;&nbsp;
-&nbsp;&nbsp; &nbsp; &nbsp;<span style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; display: inline ! important; float: none;">//
-e.g. Toronto,Canada</span>&nbsp;&nbsp; <br>
-Units:&nbsp;<big> &nbsp; &nbsp;&nbsp;&nbsp;
-&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;<select name="units"><option>Metric</option><option>Imperial</option></select>
-</big><br>
-Stock1:&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;
-&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;<input name="stock1"> &nbsp;&nbsp;&nbsp;
-&nbsp;&nbsp;&nbsp; &nbsp;<span style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; display: inline ! important; float: none;">//
-e.g. IBM</span> &nbsp; &nbsp;&nbsp;&nbsp; <br>
-Stock2:&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
-&nbsp;&nbsp;&nbsp; &nbsp; &nbsp;<input name="stock2"> &nbsp;&nbsp;&nbsp;
-&nbsp;&nbsp; &nbsp; // e.g. GOOG<br>
-<br>
+<form method="post" action="process_db.php" name="add"><table style="text-align: left; width: 567px; height: 173px;" border="1" cellpadding="2" cellspacing="2"><tbody><tr><td>Serial
+Number:&nbsp;&nbsp;</td><td><input name="serial_num"></td><td><span style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; display: inline ! important; float: none;">&nbsp;12-digit serial number on the watchback</span></td></tr><tr><td>
+Location:&nbsp;&nbsp;</td><td><input name="location"></td><td><span style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; display: inline ! important; float: none;">&nbsp;e.g. Toronto,Canada</span>&nbsp;</td></tr><tr><td>
+Units:&nbsp;</td><td><big><select name="units"><option>Metric</option><option>Imperial</option></select>
+</big></td><td>&nbsp;For  °C or °F</td></tr><tr><td>
+Stock1:&nbsp;</td><td><input name="stock1"></td><td><span style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; display: inline ! important; float: none;">&nbsp;e.g. IBM</span> &nbsp; &nbsp;&nbsp;&nbsp;</td></tr><tr><td>
+Stock2:</td><td><input name="stock2"></td><td> &nbsp;e.g. GOOG</td></tr></tbody></table><br>
 <input name="submit" value="Submit" type="submit"><br>
 </form>
-<b style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; letter-spacing: normal; line-height: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px;"><br>
+<b style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; letter-spacing: normal; line-height: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px;"><br>Please enter two stock picks.<br>
 Please use "http://finance.yahoo.com" to lookup valid stock symbols.<br>
 <br>
 <br>
