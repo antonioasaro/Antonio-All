08de418bcc2e7fed987d0b1cbd1d8ba575fc2457
antonioasaro <antonio_asaro@hotmail.com>
2013-09-22 13:07:05 -0400

    Formatting ...

--- server/index_db.php
+++ server/index_db.php
@@ -1,3 +1,39 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html><head><meta content="text/html; charset=ISO-8859-1" http-equiv="content-type"><title>test</title></head>
-<body style="background-color: white; color: rgb(0, 0, 0);" alink="#ee0000" link="#0000ee" vlink="#551a8b"><span style="font-weight: bold; color: red;"><img style="width: 117px; height: 87px;" alt="icon" src="pebble_smartwatch.jpg"><br><span style="color: black; background-color: white;">Input your personal settings for Antonio-All watchface ...</span></span><br><br><form method="post" action="process_db.php" name="add">Serial Number:&nbsp;&nbsp; &nbsp;<input name="serial_num"> &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; <span style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; display: inline ! important; float: none;">// 12-digits serial number on the watchback</span><br>Location:&nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; <input name="location">&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp;<span style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; display: inline ! important; float: none;">// e.g. Toronto,Canada</span>&nbsp;&nbsp; <br>Units:&nbsp;<big> &nbsp; &nbsp;&nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp;<select name="units"><option>Metric</option><option>Imperial</option></select></big><br>Stock1:&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;<input name="stock1"> &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;<span style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; display: inline ! important; float: none;">// e.g. IBM</span> &nbsp; &nbsp;&nbsp;&nbsp; <br>Stock2:&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp; &nbsp;<input name="stock2"> &nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp; // e.g. GOOG<br><br><input name="submit" value="Submit" type="submit"><br></form><b style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; letter-spacing: normal; line-height: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px;"><br>Please use "http://finance.yahoo.com" to lookup valid stock symbols.<br><br><br><br></b></body></html>
+<html><head><meta content="text/html; charset=ISO-8859-1" http-equiv="content-type"><title>Antonio All Settings</title>
+
+
+</head>
+<body style="background-color: white; color: rgb(0, 0, 0);" alink="#ee0000" link="#0000ee" vlink="#551a8b">
+<span style="font-weight: bold; color: red;"><img style="width: 117px; height: 87px;" alt="icon" src="pebble_smartwatch.jpg"><br>
+<span style="color: black; background-color: white;">Input
+your personal settings for the&nbsp;Antonio All watchface ...</span></span><br>
+<br>
+<form method="post" action="process_db.php" name="add">Serial
+Number:&nbsp;&nbsp; &nbsp;<input name="serial_num">
+&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+<span style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; display: inline ! important; float: none;">//
+12-digits serial number on the watchback</span><br>
+Location:&nbsp;&nbsp;&nbsp; &nbsp; &nbsp;
+&nbsp;&nbsp; &nbsp; <input name="location">&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp; &nbsp; &nbsp;<span style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; display: inline ! important; float: none;">//
+e.g. Toronto,Canada</span>&nbsp;&nbsp; <br>
+Units:&nbsp;<big> &nbsp; &nbsp;&nbsp;&nbsp;
+&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;<select name="units"><option>Metric</option><option>Imperial</option></select>
+</big><br>
+Stock1:&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;
+&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;<input name="stock1"> &nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp; &nbsp;<span style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; display: inline ! important; float: none;">//
+e.g. IBM</span> &nbsp; &nbsp;&nbsp;&nbsp; <br>
+Stock2:&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp; &nbsp; &nbsp;<input name="stock2"> &nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp; &nbsp; // e.g. GOOG<br>
+<br>
+<input name="submit" value="Submit" type="submit"><br>
+</form>
+<b style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; letter-spacing: normal; line-height: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px;"><br>
+Please use "http://finance.yahoo.com" to lookup valid stock symbols.<br>
+<br>
+<br>
+<br>
+</b>
+</body></html>
