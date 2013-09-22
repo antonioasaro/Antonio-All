456a91ec9d8db8e87d98c0e764fe7665d0cbfcf4
antonioasaro <antonio_asaro@hotmail.com>
2013-09-22 17:43:25 -0400

    Use style sheets

--- server/index_db.php
+++ server/index_db.php
@@ -1,23 +1,20 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html><head><meta content="text/html; charset=ISO-8859-1" http-equiv="content-type"><title>Antonio All Settings</title></head>
+<html><head><meta content="text/html; charset=ISO-8859-1" http-equiv="content-type"><title>Antonio All Settings</title><link rel="stylesheet" href="index_db.css" type="text/css"></head>
 <body style="background-color: white; color: rgb(0, 0, 0);" alink="#ee0000" link="#0000ee" vlink="#551a8b">
-<span style="font-weight: bold; color: red;"><img style="width: 117px; height: 87px;" alt="icon" src="pebble_smartwatch.jpg"><br><br>
-<span style="color: black; background-color: white;">Input
-your personal settings for the <small>"</small>Antonio All" watchface ...</span></span><br>
-<br>
-<form method="post" action="process_db.php" name="add"><table style="text-align: left; width: 567px; height: 173px;" border="1" cellpadding="2" cellspacing="2"><tbody><tr><td>Serial
-Number:&nbsp;&nbsp;</td><td><input name="serial_num"></td><td><span style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; display: inline ! important; float: none;">&nbsp;12-digit serial number on the watchback</span></td></tr><tr><td>
-Location:&nbsp;&nbsp;</td><td><input name="location"></td><td><span style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; display: inline ! important; float: none;">&nbsp;e.g. Toronto,Canada</span>&nbsp;</td></tr><tr><td>
-Units:&nbsp;</td><td><big><select name="units"><option>Metric</option><option>Imperial</option></select>
-</big></td><td>&nbsp;For  °C or °F</td></tr><tr><td>
-Stock1:&nbsp;</td><td><input name="stock1"></td><td><span style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; display: inline ! important; float: none;">&nbsp;e.g. IBM</span> &nbsp; &nbsp;&nbsp;&nbsp;</td></tr><tr><td>
+<img style="width: 146px; height: 107px;" alt="icon" src="pebble_smartwatch.jpg"><br><h1>Input
+your personal settings for the "Antonio All" watchface.</h1><br><form method="post" action="process_db.php" name="add"><table style="text-align: left; width: 567px; height: 173px;" border="1" cellpadding="2" cellspacing="2"><tbody><tr><td>Serial
+Number:&nbsp;&nbsp;</td><td><input name="serial_num"></td><td>&nbsp;12-digit serial number on the watchback</td></tr><tr><td>
+Location:&nbsp;&nbsp;</td><td><input name="location"></td><td>&nbsp;e.g. Toronto,Canada&nbsp;</td></tr><tr><td>
+Units:&nbsp;</td><td>
+<select name="units"><option>Metric</option><option>Imperial</option></select><br></td><td>&nbsp;For  °C or °F</td></tr><tr><td>
+Stock1:&nbsp;</td><td><input name="stock1"></td><td>&nbsp;e.g. IBM &nbsp; &nbsp;&nbsp;&nbsp;</td></tr><tr><td>
 Stock2:</td><td><input name="stock2"></td><td> &nbsp;e.g. GOOG</td></tr></tbody></table><br>
 <input name="submit" value="Submit" type="submit"><br>
 </form>
-<b style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; letter-spacing: normal; line-height: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px;"><br>Please enter two stock picks.<br>
-Please use "http://finance.yahoo.com" to lookup valid stock symbols.<br>
+<br><h2>Please enter two stock picks.</h2><h2>
+Please use "http://finance.yahoo.com" to lookup valid stock symbols.</h2>
 <br>
 <br>
-<br>
+<b style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; letter-spacing: normal; line-height: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px;"><br>
 </b>
 </body></html>
